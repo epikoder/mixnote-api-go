17891301575b477ce3e0f393a14b9b23a94a7f5d
epikoder <efedua.bell@gmail.com>
2021-07-03 02:35:31 +0100

    Updated migration * Reset database * Reset table for specific model

--- cmd/migrate/migrate.go
+++ cmd/migrate/migrate.go
@@ -1,20 +1,75 @@
 package migrate
 
 import (
+	"os"
+	"github.com/mixnote/mixnote-api-go/cmd/helper"
 	"github.com/mixnote/mixnote-api-go/src/framework/database"
 	"github.com/mixnote/mixnote-api-go/src/framework/database/migrations"
-	"log"
+	"github.com/mixnote/mixnote-api-go/src/framework/utilities"
 )
 
-func Migrate(reset bool) {
-	db, err := database.DBConnection("")
-	if err != nil {
-		log.Fatal("Could not connect to database")
+var migrations_ map[string]migrations.IMigrations
+var argHelper helper.ArgHelper = helper.New()
+
+var reset bool = argHelper.Switch("-F")
+var conn string = argHelper.Option("conn")
+var model string = argHelper.Option("model")
+
+func init() {
+	db, _ := database.DBConnection(conn)
+	migrations_ = map[string]migrations.IMigrations{
+		"user": migrations.CreateUserTable(db),
+		"role": migrations.CreateRoleTable(db),
+	}	
+}
+
+func Migrate() {
+	if reset {
+		resetDB()
+		os.Exit(0)
 	}
 
-	migrations.CreateUserTable().Up(db)
+	if model != "" {
+		m_ := migrations_[model]
+		if m_ == nil {
+			utilities.Console().Fatal("Model not found")
+		}
 
-	db.Migrator().DropTable("user")
+		err := m_.Down()
+		if err != nil {
+			utilities.Console().Error("Unable to drop table for model " + model)
+			utilities.Console().Fatal(err)
+		}
+		
+		utilities.Console().Warn("Migrating table for model " + model)
+		err = m_.Up()
+		if err != nil {
+			utilities.Console().Error("Unable to migrate table for model " + model)
+			utilities.Console().Fatal(err)
+		}
+		utilities.Console().Success("Migrated table for model " + model)
+		os.Exit(0)
+	}
+}
 
-	// db.AutoMigrate(&album.Album{})
+func resetDB() {
+	for key, m_ := range migrations_ {
+		err := m_.Down()
+		if err != nil {
+			utilities.Console().Error("Unable to drop table for model " + key)
+			utilities.Console().Fatal(err)
+		}
+	}
+	utilities.Console().Warn("Dropped All tables")
+
+
+	for key, m_ := range migrations_ {
+		utilities.Console().Warn("Migrating table for model " + key)
+		err := m_.Up()
+		if err != nil {
+			utilities.Console().Error("Unable to migrate table for model " + key)
+			utilities.Console().Fatal(err)
+		}
+		utilities.Console().Success("Migrated table for model " + key)
+	}
 }
