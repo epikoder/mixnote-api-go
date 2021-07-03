3bff4f455862d72acd74d9262da6d0e173d53099
epikoder <efedua.bell@gmail.com>
2021-07-03 02:37:44 +0100

    Update user model

--- src/core/models/user.go
+++ src/core/models/user.go
@@ -1,24 +1,33 @@
 package models
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
+	"github.com/mixnote/mixnote-api-go/src/music/model"
 )
 
 type (
 	User struct {
-		gorm.Model
+		ID		uuid.UUID `sql:"primary_key;type:uuid;default:uuid_generate_v4()"`
+		FirstName string    `gorm:"type:char[50]"`
+		LastName  string	`gorm:"type:char(50)"`
+		Email     string	`gorm:"type:char(150)"`
+		Phone     int		`gorm:"type:int(18)"`
+		CreatedAt	time.Time
+		UpdatedAt	time.Time
+		DeletedAt	gorm.DeletedAt	`gorm:"index"`
 
-		ID     uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
-		FirstName string `json:"first_name"`
-		LastName string
-		Email string
-		Phone int
+		Artist    music_models.Artist
 	}
 )
 
+func (u *User) BeforeCreate(*gorm.DB) error {
+	return nil
+}
+
 func New_User() (u *User) {
-	u = &User{}
 	return
 }
 
