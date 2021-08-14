0a82479706cee81fa5111f90f2f06b6134d53249
epikoder <efedua.bell@gmail.com>
2021-08-15 00:31:02 +0100

    Update configs

--- configs/0_.go
+++ configs/0_.go
@@ -9,9 +9,11 @@ import (
 
 var __PATH__, _ = os.Getwd()
 var __DB_PATH__ = __PATH__ + "/bin/database.sqlite"
+
 func init() {
 	envPath := __PATH__ + "/.env"
 	if err := godotenv.Load(envPath); err != nil {
 		fmt.Printf("Could not load .env file in %s", envPath)
 	}
+	os.Setenv("ENV_PATH", envPath)
 }
--- configs/app.go
+++ configs/app.go
@@ -1,17 +1,18 @@
 package configs
 
 import (
-	"fmt"
 	"os"
 	"sync"
 )
 
-type app struct{
-	Name string
-	Path string
+type app struct {
+	Name         string
+	Path         string
 	DatabasePath string
-	Env string
-	Local bool
+	Env          string
+	Local        bool
+	DEBUG        bool
+	LOG_STACK     string
 }
 
 var once sync.Once
@@ -19,18 +20,26 @@ var App *app
 
 func init() {
 	once.Do(func() {
-		var name string
-		var env string
-		if name =  os.Getenv("APP_NAME"); name == "" {name = "Soro"}
-		if env = os.Getenv("APP_ENV"); env == "" {env = "production"}
-		
-		fmt.Println(os.Getenv("APP_NAME") + " From configs")
+		var name, env string
+		var debug bool
+		if name = os.Getenv("APP_NAME"); name == "" {
+			name = "Soro"
+		}
+		if env = os.Getenv("APP_ENV"); env == "" {
+			env = "production"
+		}
+		if debug_ := os.Getenv("APP_DEBUG"); debug_ == "" {
+			debug = false
+		}
+
 		App = &app{
-			Name: name,
-			Path: __PATH__,
+			Name:         name,
+			Path:         __PATH__,
 			DatabasePath: __DB_PATH__,
-			Env: env,
-			Local: env != "production",
+			Env:          env,
+			Local:        env != "production",
+			DEBUG:        debug,
+			LOG_STACK:  os.Getenv("LOG_STACK"),
 		}
 	})
 }
