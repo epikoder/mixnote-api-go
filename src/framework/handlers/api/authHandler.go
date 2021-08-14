54a4d8f9b1ea9c06377315414b64d235a7fd8ac3
epikoder <efedua.bell@gmail.com>
2021-08-15 00:50:34 +0100

    Updated Register and Login handler

--- src/framework/handlers/api/authHandler.go
+++ src/framework/handlers/api/authHandler.go
@@ -1,25 +1,88 @@
 package api_handlers
 
 import (
+	"encoding/json"
 	"net/http"
-
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
+	"github.com/mixnote/mixnote-api-go/src/core/models"
+	"github.com/mixnote/mixnote-api-go/src/framework/hasher"
+	"github.com/mixnote/mixnote-api-go/src/framework/repositories"
+	"github.com/mixnote/mixnote-api-go/src/framework/validator"
 )
 
 type (
-	auth struct {}
+	auth struct{}
+)
+
+var (
+	u *models.User = &models.User{}
 )
 
 func Auth() (a *auth) {
 	return
 }
 
+// API Route login
 func (*auth) LoginEmail(ctx echo.Context) error {
-	return ctx.String(http.StatusOK, "Hello Login page")
+	rules := map[string][]string{
+		"email":    {"email"},
+		"password": {"required", "alpha_numeric", "min:8"},
+	}
+	form, bag, err := validator.NewValidator(ctx.Request(), rules, nil).Validate()
+	if err != nil {
+		return ctx.String(http.StatusInternalServerError, err.Error())
+	}
+	
+	if bag.Failed() {
+		return ctx.JSON(http.StatusBadRequest, bag)
+	}
+
+	if ok := repositories.UserRepository(u).FindByEmail(form.GetString("email")); !ok {
+		return ctx.String(http.StatusUnauthorized, "user does not exist")
+	}
+
+	if ok := hasher.Check(u.UserCredential.Password, form.GetString("password")); !ok {
+		return ctx.String(http.StatusUnauthorized, "password does not match")
+	}
+
+	return ctx.JSON(http.StatusOK, map[string]interface{}{
+		"user": u,
+	})
 }
 
-func (*auth) RegisterEmail(ctx echo.Context) error {
-	return ctx.JSON(http.StatusOK, map[string]interface{}{
-		"user": 1,
+func (*auth) RegisterEmail(ctx echo.Context) (err error) {
+	rules := map[string][]string{
+		"email":    {"email"},
+		"password": {"required", "alpha_numeric", "min:8"},
+	}
+	form, bag, err := validator.NewValidator(ctx.Request(), rules, nil).Validate()
+	if err != nil {
+		return ctx.String(http.StatusInternalServerError, err.Error())
+	}
+	
+	if bag.Failed() {
+		return ctx.JSON(http.StatusBadRequest, bag)
+	}
+
+	//
+	if ok := repositories.UserRepository(u).FindByEmail(form.GetString("email")); ok {
+		return ctx.String(http.StatusOK, "user already exist")
+	}
+	p, err := hasher.Make(form.GetString("password"))
+	if err != nil {
+		return ctx.String(http.StatusInternalServerError, "Server error")
+	}
+	repositories.UserRepository(u).CreateUser(&models.User{
+		ID:    uuid.New(),
+		Email: form.GetString("email"),
+		UserCredential: models.UserCredential{
+			ID:       uuid.New(),
+			Password: p,
+		},
 	})
+	return ctx.String(http.StatusOK, (func() string {
+		n, _ := json.Marshal(u)
+		return string(n)
+	})())
 }
