9b2685667b4c87ab55e7b337e66435aff34e6565
epikoder <efedua.bell@gmail.com>
2021-10-13 14:43:35 +0100

    Updated  app

--- src/app.go
+++ src/app.go
@@ -1,11 +1,15 @@
 package app
 
 import (
+	"github.com/mixnote/mixnote-api-go/src/core/service/totem"
 	"github.com/mixnote/mixnote-api-go/src/framework/router"
 )
 
 // Bootstrap
-func init() {}
+func init() {
+	totem.UseRedis()
+	totem.AccessExpiresIn(60*24*7) //TODO remove this
+}
 
 func StartServer(host string, port int) {
 	server.Serve(host, port)
